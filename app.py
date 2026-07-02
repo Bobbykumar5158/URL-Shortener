ae18d6433ca8deafeb9868c9ec07aa0f976b59ad
BOBBY KUMAR <lpsbobbykumar5158@gmail.com>
2026-07-03 00:02:38 +0530

    Created navbar

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask , render_template
+from flask import Flask , render_template ,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/api")
+def api():
+    return render_template("api.html")
+
+@app.route("/api/links")
+def api_links():
+    return jsonify({"data":{"key":"val"}}) # only for example will modify after
 
 if __name__ == "__main__":
     app.run(debug = True)
